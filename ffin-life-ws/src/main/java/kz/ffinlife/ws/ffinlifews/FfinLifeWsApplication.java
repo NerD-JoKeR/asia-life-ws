856554658dd9923b85177416aac92e575de70775
NerD-JoKeR <kanjar_95@mail.ru>
2019-04-05 10:24:35 +0600

    added class ConnectorConfig web-service version 2.0

---
+++ ffin-life-ws/src/main/java/kz/ffinlife/ws/ffinlifews/FfinLifeWsApplication.java
@@ -0,0 +1,13 @@
+package kz.ffinlife.ws.ffinlifews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfinLifeWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfinLifeWsApplication.class, args);
+	}
+
+}
